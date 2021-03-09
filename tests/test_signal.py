c1ad859d5504fb81d505e8321c5387e9c19096a1
Robin Allesiardo <robin.allesiardo@orange.com>
2021-03-09 14:00:37 +0100

    Initialization of the repository

---
+++ tests/test_signal.py
@@ -0,0 +1,31 @@
+from signal import Signal, signal
+
+signals = [Signal, signal]
+
+def test_signal():
+    
+    class Logger():
+        """
+        A  class to log objects passed to self.log(x) into the list self.data
+        """
+        def __init__(self):
+            self.data = []
+            
+        def log(self, x):
+            self.list.append(x)
+            
+            
+    for i in range(len(signals)):
+        """ Test for both the Signal class and the proxy signal"""
+        
+        i_signal = signals[i]() # create the signal
+        logger = Logger() # create the logger
+        i_signal.connect(logger.log()) # connect the signal to the logger
+        
+        i_signal.emit("stuff")
+        i_signal.emit("thing")
+        
+        assert foo.data == ["stuff", "thing"] # check if everything was logged
+        
+
+
